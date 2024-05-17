090bc3a345bca2df2a109b44aa211aac80b2ce42
mainVerstak <54151544+mainVerstak@users.noreply.github.com>
2024-05-17 15:22:23 +0300

    cookie fix

--- js/script.js
+++ js/script.js
@@ -2,12 +2,49 @@
 
 document.addEventListener("DOMContentLoaded", function () {
 
+  //+cookie
+  function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + "; SameSite=None; Secure; path=/";
+  }
+  (function checkCookie() {
+    let cookieA = getCookie("cookie_accepted");
+    if (cookieA == "") {
+      document.querySelectorAll('.popup-cookie').forEach(function (popup) {
+        popup.classList.add('_active')
+      });
+    }
+  })();
   document.querySelectorAll('.js-cookie-accept').forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+      let popup = btn.closest('.popup-cookie');
+      popup.classList.remove('_active');
+      setCookie("cookie_accepted", "true", 365);
+    })
+  });
+  document.querySelectorAll('.js-cookie-close').forEach(function (btn) {
     btn.addEventListener('click', function (e) {
       let popup = btn.closest('.popup-cookie');
       popup.classList.remove('_active');
     })
   });
+  //-cookie
 
   //+float header 
   let header = document.querySelector('.header');
